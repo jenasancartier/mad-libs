a5767e738a0efba3492a0b48626b757a130d0f56
Jena SanCartier and Katrina Hirsch <jena@>
2016-08-16 11:40:00 -0700

    change forEach to for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
+//FOR LOOP
 $(document).ready(function() {
 
   $("#blanks form").submit(function(event) {
     var blanks = ["person1", "person2", "adjective", "exclamation", "noun", "verb"];
+    for (var index = 0; index < blanks.length; index += 1) {
+      var userInput = $("input#" + blanks[index]).val();
+      $("." + blanks[index]).text(userInput);
+      $("#story").show();
 
-    blanks.forEach(function(blank){
-      var userInput = $("input#" + blank).val();
-      $("." + blank).text(userInput);
-    });
+      event.preventDefault();
+    }
+
+//FOR EACH LOOP
+    // blanks.forEach(function(blank){
+    //   var userInput = $("input#" + blank).val();
+    //   $("." + blank).text(userInput);
+    // });
 
 
-    //
+//ORIGINAL
     // var person1Input = $("input#person1").val();
     // var person2Input = $("input#person2").val();
     // var adjectiveInput = $("input#adjective").val();
@@ -24,8 +33,8 @@ $(document).ready(function() {
     // $(".noun").text(nounInput);
     // $(".verb").text(verbInput);
 
-    $("#story").show();
-
-    event.preventDefault();
+    // $("#story").show();
+    //
+    // event.preventDefault();
   });
 });
